e4bfe790a735ef94cd21f5abcd53c267dc69442c
blurryt <matheusc2005@hotmail.com>
2025-06-04 21:15:34 -0300

    corrige sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+echo ">>> Instalando dependências"
+
+echo ">>> Criando banco de dados (se necessário)"
 pip install -r requirements.txt
-python preenche_estatico.py
-python ./importar_csv.py
+
+echo ">>> Preenchendo banco com dados estáticos e CSV"
+python -c "from importar_csv import carregar_csv; carregar_csv()"
+python -c "from preenche_estatico import preenche_estatico; preenche_estatico()"
