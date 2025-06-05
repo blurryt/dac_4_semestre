5f43a8d955fe0e70ebc8f299ddaab67a2d1fc46c
blurryt <matheusc2005@hotmail.com>
2025-06-04 21:08:52 -0300

    add sh

--- app_factory.py
+++ app_factory.py
@@ -13,17 +13,17 @@ def init_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DB_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    @app.before_request
-    def inicializar_banco():
-        global carregamento_feito
-        if not carregamento_feito:
-            from models import Escola
-            from importar_csv import carregar_csv
-            from preenche_estatico import preenche_estatico 
+    # @app.before_request
+    # def inicializar_banco():
+    #     global carregamento_feito
+    #     if not carregamento_feito:
+    #         from models import Escola
+    #         from importar_csv import carregar_csv
+    #         from preenche_estatico import preenche_estatico 
             
-            if not Escola.query.first():
-                print(">> Populando banco pela primeira vez...")
-                carregar_csv()
-                preenche_estatico()
-            carregamento_feito = True
+    #         if not Escola.query.first():
+    #             print(">> Populando banco pela primeira vez...")
+    #             carregar_csv()
+    #             preenche_estatico()
+    #         carregamento_feito = True
     return app
---
+++ build.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+python preenche_estatico.py
+python ./importar_csv.py
