2874d8b13e335ce64b22cbd889ae81e01b51a694
blurryt <matheusc2005@hotmail.com>
2025-06-04 21:27:03 -0300

    corrige sh

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ echo ">>> Instalando dependências"
 echo ">>> Criando banco de dados (se necessário)"
 pip install -r requirements.txt
 
-echo ">>> Criando banco de dados (se necessário)"
-python -c "from app import db, app; db.create_all()"
+echo ">>> Criando tabelas no banco"
+python -c "from app_factory import init, db; app = init(); from models import *; with app.app_context(): db.create_all()"
+
+echo ">>> Preenchendo banco"
+python init_db.py
 
 echo ">>> Preenchendo banco com dados estáticos e CSV"
 python -c "from importar_csv import carregar_csv; carregar_csv()"
