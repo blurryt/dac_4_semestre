cc16445cc7834574c91bfb4b176fa82c3502cab8
blurryt <matheusc2005@hotmail.com>
2025-06-06 21:33:43 -0400

    add cors

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ echo ">>> Instalando dependências"
 echo ">>> Criando banco de dados (se necessário)"
 pip install -r requirements.txt
 
-echo ">>> Inicializando banco de dados"
-python init_deploy.py
+#echo ">>> Inicializando banco de dados"
+#python init_deploy.py
